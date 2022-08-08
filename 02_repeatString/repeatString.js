f7bf8475568d6cbb84dddd70e29b1af4408fe5e6
Papa1822 <103136704+Papa1822@users.noreply.github.com>
2022-08-08 22:36:19 +0000

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+  if(times > 0){
+    return string.repeat(times);
+  }else{
+    return "";
+  }
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
